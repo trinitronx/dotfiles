d38a48d91913b456c3fb566d34e37588252ce9d7
James Cuzella <james.cuzella@lyraphase.com>
2024-08-08 01:34:33 -0600

    .config/sway: Add sway-sensors nwg-wrapper scripts + binding

---
+++ private_dot_local/private_share/sway/scripts/executable_sway-sensors-toggle.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright (C) © 🄯  2023-2024 James Cuzella
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+sway_sensors_start_cmd="${XDG_DATA_HOME:-${HOME}/.local/share}/sway/scripts/sway-sensors-show.sh"
+sway_sensors_kill_cmd="${XDG_DATA_HOME:-${HOME}/.local/share}/sway/scripts/sway-sensors-hide.sh"
+sensors_overlay_state=/tmp/sway-sensors.state
+
+if pgrep --full  'nwg-wrapper -s sway-sensors.sh' 1>/dev/null 2>&1 ; then
+  swaylock_time_pid=$(pgrep --full  'nwg-wrapper -s sway-sensors.sh')
+  if [ -f "$sensors_overlay_state" ]; then
+    state=$(cat "$sensors_overlay_state")
+    if [[ "$state" -eq "1" ]]; then
+      # toggle to overlay
+      pkill --full --signal 10  "nwg-wrapper -s sway-sensors.sh.*--sig_quit 31"
+      echo -n "3" > $sensors_overlay_state
+    else
+      $sway_sensors_kill_cmd
+    fi
+  else
+    # Assume it's in state 1 and toggle until killed
+    echo -n "1" > $sensors_overlay_state
+  fi
+else
+  $sway_sensors_start_cmd && echo -n "1" > $sensors_overlay_state
+fi

2a4b4323459f82d53896897c39469239ff088eda
James Cuzella <james.cuzella@lyraphase.com>
2024-09-07 16:25:55 -0600

    .config/zsh: Add chezmoi development helper function: chezmoi-git-apply-check

---
+++ dot_config/zsh/config.d/03-chezmoi-fxns.zsh
@@ -0,0 +1,27 @@
+#!/usr/bin/zsh
+# Author: James Cuzella <james.cuzella@lyraphase.com>
+# License: GNU AGPLv3
+
+# Copyright (C) © 🄯  2024 James Cuzella
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+function chezmoi-git-apply-check() {
+  git diff --name-only $(git oldest-ancestor main)..HEAD | \
+    grep -v .chezmoi | \
+    sed -e 's/dot_/\./g' -e 's/executable_//g' \
+        -e 's/\.tmpl$//g' -e 's/symlink_//g' \
+        -e 's/private_//g' -e 's#^#~/#' | \
+    xargs chezmoi apply --verbose --dry-run --interactive
+}
+

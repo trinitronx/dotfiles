2051f9ebe75ca9b0c38fca28728a3dd6d1379b4c
James Cuzella <james.cuzella@lyraphase.com>
2025-12-27 13:16:10 -0700

    .config/profile.d/00-xdg-data-dirs.sh: Set per-OS default for XDG_DATA_DIRS & handle prepending flatpak dirs

--- dot_config/profile.d/executable_00-xdg-data-dirs.sh
+++
@@ -1,5 +0,0 @@
-case ":${XDG_DATA_DIRS}:" in
-  *:"${XDG_DATA_HOME:-$HOME/.local/share}":*) : ;; # no-op
-  *) export XDG_DATA_DIRS="${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS:-/usr/share:/usr/local/share}" ;;
-esac
-

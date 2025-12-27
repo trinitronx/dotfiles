647035d305afd78ec820190c4ca7c843d64f09fa
James Cuzella <james.cuzella@lyraphase.com>
2025-12-27 12:52:19 -0700

    .config/profile.d/00-xdg-data-dirs.sh: Fix idempotency when prepending to XDG_DATA_DIRS

--- dot_config/profile.d/executable_00-xdg-data-dirs.sh
+++ dot_config/profile.d/executable_00-xdg-data-dirs.sh
@@ -1,2 +1,5 @@
-export XDG_DATA_DIRS="${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS}"
+case ":${XDG_DATA_DIRS}:" in
+  *:"${XDG_DATA_HOME:-$HOME/.local/share}":*) : ;; # no-op
+  *) export XDG_DATA_DIRS="${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS}" ;;
+esac
 

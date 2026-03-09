2088f2cff15f18deec88e03d3b2e25afba628196
James Cuzella <james.cuzella@lyraphase.com>
2026-03-09 15:17:43 -0600

    config/zsh/config.d: Add AGPL notice to fxns-fzf.zsh

--- dot_config/zsh/config.d/fxns-fzf.zsh
+++ dot_config/zsh/config.d/fxns-fzf.zsh
@@ -1,3 +1,13 @@
+# Copyright (C) © 🄯  2024-2026 James Cuzella
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+
 __view_function_code() {
   # A POSIX Regex to filter out cluttered hidden Zsh functions from the list
   __FXNS_GREP_FILTER_SYSTEM_FXNS_REGEX='(^(_|\+vi|azhw:|instant_prompt|mzc_|comp|omz|p10k|p9k|prompt_)|(_p9k_)$)'

ae8b9abfe4a708a41845c83eaa242f5e8af2470c
James Cuzella <james.cuzella@lyraphase.com>
2026-05-02 09:00:27 -0600

    .local/bin/imv-last-screenshot.sh: Fix last screenshot detection - ignore .goutputstream

--- private_dot_local/bin/executable_imv-last-screenshot.sh
+++ private_dot_local/bin/executable_imv-last-screenshot.sh
@@ -17,6 +17,8 @@
 busctl --json=short --user call  org.freedesktop.Notifications /fr/emersion/Mako fr.emersion.Mako ListHistory \
   | jq -r '.data | .[] |
     map(
-      select(.summary.data == "Screenshot saved")
+      select(.summary.data == "Screenshot saved"
+             and (.body.data | contains(".goutputstream") | not )
+      )
     ) | first | .body.data' | \
   imv
